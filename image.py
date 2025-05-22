9b30768d4c838d0943bc54868725fa5879c9b625
Sakshi Sihag <sakshisihag20@gmail.com>
2025-05-22 18:42:45 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,16 +1,22 @@
 from PIL import Image, ImageFilter
+
+# Pikachu - Sharpen
 img = Image.open('./pokedox/pikachu.jpg')
 sharp_pikachu = img.filter(ImageFilter.SHARPEN)
-img.save("sharp_pikachu", 'png')
+sharp_pikachu.save("sharp_pikachu.png")
 
+# Bulbasaur - Blur
 img = Image.open('./pokedox/bulbasaur.jpg')
 blur_bulbasaur = img.filter(ImageFilter.BLUR)
-img.save("blur_bulbasaur", 'png')
+blur_bulbasaur.save("blur_bulbasaur.png")
 
+# Squirtle - Smooth
 img = Image.open('./pokedox/squirtle.jpg')
 smooth_squirtle = img.filter(ImageFilter.SMOOTH)
-img.save("smooth_squirtle" , 'png')
+smooth_squirtle.save("smooth_squirtle.png")
 
+# Charmander - Grayscale
 img = Image.open('./pokedox/charmander.jpg')
 grey_charmander = img.convert('L')
-grey_charmander.save("grey_charmander", 'png')
+grey_charmander.save("grey_charmander.png")
+
